e040d64b183aca442c39a9e360cdca8bd544a77d
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2023-03-07 21:16:57 +0300

    last update

--- secuirty/app.py
+++ secuirty/app.py
@@ -1,5 +1,3 @@
-from typing import cast
-
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise import timezone
